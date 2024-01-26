30854f8120f4dbb29d54939bb41109042ea4f811
ParZival6677 <113224392+ParZival6677@users.noreply.github.com>
2024-01-26 19:14:28 +0600

    Delete server directory

--- server/routes/geolocation.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const geolocationController = require('../controllers/geolocationController');
-
-router.get('/', geolocationController.getGeolocation);
-
-module.exports = router;
--- server/routes/jokes.js
+++
@@ -1,8 +0,0 @@
-// server/routes/jokes.js
-const express = require('express');
-const router = express.Router();
-const jokesController = require('../controllers/jokesController');
-
-router.get('/random', jokesController.getRandomJoke);
-
-module.exports = router;
